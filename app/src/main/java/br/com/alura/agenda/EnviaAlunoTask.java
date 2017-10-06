40343f0ea69347e4e1aaae62f9f1f7fcc133250c
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2017-10-05 22:28:17 -0300

    FIM CURSO 02 ANDROID ALURA - WEBSERVICE

---
+++ app/src/main/java/br/com/alura/agenda/EnviaAlunoTask.java
@@ -0,0 +1,54 @@
+package br.com.alura.agenda;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.util.List;
+
+import br.com.alura.agenda.converter.AlunoConverter;
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.modelo.Aluno;
+
+/**
+ * Created by Hiro on 05/10/2017.
+ */
+
+public class EnviaAlunoTask extends AsyncTask<Void, Void, String> {
+
+    private Context context;
+    private ProgressDialog dialog;
+
+    public EnviaAlunoTask(Context context) {
+        this.context = context;
+    }
+
+
+    @Override
+    protected void onPreExecute() {
+        dialog = ProgressDialog.show(context, "Aguarde", "Enviando alunos...", true, true);
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+
+        AlunoDAO dao = new AlunoDAO(context);
+        List<Aluno> alunos = dao.buscaAlunos();
+        dao.close();
+
+        AlunoConverter conversor = new AlunoConverter();
+        String json = conversor.converteParaJSON(alunos);
+
+        WebClient client = new WebClient();
+        String resposta = client.post(json);
+
+        return resposta;
+    }
+
+    @Override
+    protected void onPostExecute(String resposta) {
+        dialog.dismiss();
+        Toast.makeText(context, resposta , Toast.LENGTH_LONG).show();
+    }
+}
--- app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -22,6 +22,7 @@ import java.util.List;
 import java.util.jar.Manifest;
 
 import br.com.alura.agenda.adapter.AlunosAdapter;
+import br.com.alura.agenda.converter.AlunoConverter;
 import br.com.alura.agenda.dao.AlunoDAO;
 import br.com.alura.agenda.modelo.Aluno;
 
@@ -76,6 +77,25 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         carregaLista();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_lista_alunos, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_enviar_notas:
+                new EnviaAlunoTask(this).execute();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
